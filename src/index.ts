8501f0af4e76a2abd8934bfc88cc439630fd6b18
SouTamura <tamuchan0209@gmail.com>
2024-12-21 00:30:47 +0900

    feat: サーバ起動コードを追加

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ const port = parseInt(process.env.PORT || "3000", 10);
 
 app.set("port", port)
 const server = http.createServer(app)
+server.listen(port);
 
 server.on("listening", ()=>{
     const address = server.address()
